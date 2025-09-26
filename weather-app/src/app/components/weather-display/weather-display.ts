492ccdf705188b39fa335793659507469f640236
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-09-26 12:37:52 +0530

    chore(init): basic project structure

---
+++ weather-app/src/app/components/weather-display/weather-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-display',
+  imports: [],
+  templateUrl: './weather-display.html',
+  styleUrl: './weather-display.css'
+})
+export class WeatherDisplay {
+
+}
